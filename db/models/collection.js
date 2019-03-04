72ce6ba988b31d6c4581545cc531ce106c071212
FX-Wood <fxwood2@gmail.com>
2019-03-04 13:46:47 -0800

    Fixed collections model to include userID

--- db/models/collection.js
+++ db/models/collection.js
@@ -1,11 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const collection = sequelize.define('collection', {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    userId: DataTypes.INTEGER
   }, {});
   collection.associate = function(models) {
     // associations can be defined here
-    models.collection.hasMany(models.card)
+    models.collection.belongsTo(models.user)
+    models.collection.hasMany(models.card);
   };
   return collection;
 };
