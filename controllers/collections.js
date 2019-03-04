6bdc005956cacf7e311bde8465005d9ce52aa208
FX-Wood <fxwood2@gmail.com>
2019-03-04 11:24:21 -0800

    Adding router boilerplate for collections

---
+++ controllers/collections.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const db = require('../db/models')
+const isLoggedIn = require('../middleware/isLoggedIn');
+const router = express.Router();
+
+router.use(isLoggedIn);
+
+// routes for collection
+
+router.route('/')
+    // GET index
+    .get((req,res) => {
+        res.send('you are at collections/index for user');
+    })
+    // POST new collection
+    .post((req,res) => {
+        res.send('you just created a collection');
+    })
+
+router.route('/:id')
+    // GET one collection
+    .get((req,res) => {
+        res.send('you are at collections/show for a single collection' + req.params.id);
+    })
+    // PUT edit one collection
+    .put((req,res) => {
+        res.send('you have edited a single collection' + req.params.id);
+    })
+    // DELETE one collection
+    .delete((req,res) => {
+        res.send('you have deleted a collection' + req.params.id);
+    })
+router.get('/new', (req,res) => {
+    res.send('this is a new collection form')
+})
+module.exports = router;
