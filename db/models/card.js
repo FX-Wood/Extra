019b016d6d676743fc610129db4dd36addaa4eb6
FX-Wood <fxwood2@gmail.com>
2019-03-05 15:38:02 -0800

    updated model to reflect migration

--- db/models/card.js
+++ db/models/card.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const card = sequelize.define('card', {
     front: DataTypes.STRING,
-    back: DataTypes.STRING
+    back: DataTypes.STRING,
+    collectionId: DataTypes.INTEGER
   }, {});
   card.associate = function(models) {
     // associations can be defined here
