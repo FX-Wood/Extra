39e8958bc4e705a12cb7185d7bfcf309bcb5bf56
FX-Wood <fxwood2@gmail.com>
2019-03-06 13:37:49 -0800

    Finished implementing add-card functionality

--- public/js/cards-new.js
+++ public/js/cards-new.js
@@ -3,17 +3,23 @@ function addCardHandler(e) {
     let front = document.getElementById('front').innerText
     let back = document.getElementById('back').innerText
     let collectionId = M.FormSelect.getInstance(document.getElementById('collection-picker')).getSelectedValues()[0]
-    // fetch('/cards', {
-    //     method: 'POST',
-    //     credentials: 'include',
-    //     headers: {
-    //         'content-type': 'application/json'
-    //     },
-    //     body: JSON.stringify({front, back})
-    // })
-    console.log(JSON.stringify({front, back, collectionId}))
+    fetch('/cards', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({front, back, collectionId})
+    }).then(reply => {
+        return reply.json()
+    }).then(newCard => {
+        console.log(newCard)
+        window.location = '/collections/' + collectionId
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function(e) {
     M.FormSelect.init(document.getElementById('collection-picker'))
+    document.getElementById('add-card-to-collection-btn')
+    .addEventListener('click', addCardHandler)
 })
