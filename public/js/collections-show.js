889a974e4ee81d352b16d3dd068eef90c5b08922
FX-Wood <fxwood2@gmail.com>
2019-03-06 13:22:12 -0800

    Extracted anonymous click handler to named function

---
+++ public/js/collections-show.js
@@ -0,0 +1,15 @@
+var deleteModal;
+
+function deleteCollectionButton(e) {
+    // url is defined in the rendering of page from server
+    fetch(url, {method: 'DELETE'})
+    .then(window.location = '/collections')
+    .catch(error => console.info(error))
+    deleteModal.close()
+}
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    deleteModal = M.Modal.init(document.querySelector('#delete-collection-modal'))
+    document.getElementById('DELETE-COLLECTION')
+    .addEventListener('click', deleteCollectionButton)
+})
