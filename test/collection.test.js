a6dec106164d18f0f8958a1bfb7bd8a5cb240b7f
FX-Wood <fxwood2@gmail.com>
2019-03-13 14:11:17 -0700

    Made test for creating collections

---
+++ test/collection.test.js
@@ -0,0 +1,71 @@
+var expect = require('chai').expect;
+var request = require('supertest');
+var app = require('../server');
+var db = require('../db/models');
+var agent = request.agent(app);
+
+before(function(done) {
+    db.sequelize.sync({ force: true }).then(function() {
+        done();
+    });
+});
+
+describe('Collections Controller', function() {
+    var AUTH_COOKIE;
+    describe('GET /collections', function() {
+        it('should redirect to /auth/login if not logged in', function(done) {
+            request(app).get('/collections')
+            .expect('Location', '/auth/login')
+            .expect(302, done);
+        });
+
+        it('should return a 200 response if logged in', function(done) {
+            agent.post('/auth/signup')
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .send({
+                email: 'test@test.com',
+                name: 'test',
+                password: 'password',
+                signupCode: process.env.SIGNUP_KEY
+            })
+            .expect(302)
+            .expect('Location', '/')
+            .end(function(error, res) {
+                if (error) {
+                    done(error);
+                } else {
+                    AUTH_COOKIE = res.header['set-cookie'][0]
+                    agent.set('Cookie', AUTH_COOKIE);
+                    agent.get('/collections')
+                    .expect(200, done);
+                }
+            })
+        })
+    });
+
+    describe('POST /collections', function() {
+        it('should create a new collection', async function(done) {
+            // count collections
+            let count = await db.collection.count()
+            agent.post('/collections')
+                .set('Cookie', AUTH_COOKIE)
+                .send()
+                .end(function(error, res) {
+                    db.collection.count()
+                    .then(newCount => {
+                        if (count < newCount) {
+                            done()
+                        }
+                    }).catch(function(error) {
+                        done(error)
+                    })
+                })
+        })
+    })
+
+    // describe('DELETE /collections', function() {
+    //     it('should delete a collection', async function(done) {
+    //         //
+    //     })
+//     })
+})
