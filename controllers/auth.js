1ad7efe3189f7e94c65ed67f95d11e14407d6c03
FX Wood <fxwood.work@gmail.com>
2024-09-01 21:26:26 -0700

    update passport to 0.7

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,60 +1,69 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db/models');
-const passport = require('../config/passportConfig');
+const db = require("../db/models");
+const passport = require("../config/passportConfig");
 
-router.get('/signup', function(req, res) {
-  res.render('auth/signup', { user: req.user });
+router.get("/signup", function (req, res) {
+  res.render("auth/signup", { user: req.user });
 });
 
-router.post('/signup', function(req,res) {
+router.post("/signup", function (req, res) {
   if (!req.signupCode === process.env.SIGNUP_KEY) {
-    res.status(301).json(Error('Incorrect sign-up key'))
-    throw Error('Incorrect sign-up key')
+    res.status(301).json(Error("Incorrect sign-up key"));
+    throw Error("Incorrect sign-up key");
   } else {
-    db.user.findOrCreate({
-      where: {email: req.body.email},
-      defaults: {
-        name: req.body.name,
-        password: req.body.password
-      }
-    }).spread(function(user, created) {
-      console.log('In signup, user created: ', created)
-      if (created) {
-        passport.authenticate('local', {
-          successRedirect: '/',
-          successFlash: 'Account created and logged in'
-        })(req, res)
-      } else {
-        console.log('Email already exists');
-        req.flash('error', 'email already exists');
+    db.user
+      .findOrCreate({
+        where: { email: req.body.email },
+        defaults: {
+          name: req.body.name,
+          password: req.body.password,
+        },
+      })
+      .spread(function (user, created) {
+        console.log("In signup, user created: ", created);
+        if (created) {
+          passport.authenticate("local", {
+            successRedirect: "/",
+            successFlash: "Account created and logged in",
+          })(req, res);
+        } else {
+          console.log("Email already exists");
+          req.flash("error", "email already exists");
+          res.redirect("/auth/signup");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+        req.flash("error", error.errors[0].message);
         res.redirect("/auth/signup");
-      }
-    }).catch(function(error) {
-      console.log(error)
-      req.flash('error', error.errors[0].message)
-      res.redirect('/auth/signup')
-    })
+      });
   }
-})
-
-router.get('/login', function(req, res) {
-  res.render('auth/login', { user: req.user });
 });
 
-router.post('/login', passport.authenticate('local', {
-  successFlash: 'You have logged in!',
-  failureRedirect: '/auth/login',
-  failureFlash: 'Invalid credentials'
+router.get("/login", function (req, res) {
+  res.render("auth/login", { user: req.user });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successFlash: "You have logged in!",
+    failureRedirect: "/auth/login",
+    failureFlash: "Invalid credentials",
   }),
-  (req, res) => res.render('index', { user:req.user })
+  (req, res) => res.render("index", { user: req.user }),
 );
 
-router.get('/logout', function(req, res) {
-  req.logout();
-  req.user = null;
-  req.flash('success', 'You have logged out!');
-  res.redirect('/')
-})
+router.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.user = null;
+    req.flash("success", "You have logged out!");
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
