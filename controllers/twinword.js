5e6bc5685838b170de194de2f335b64ac9f1a826
FX Wood <fxwood.work@gmail.com>
2024-09-02 12:33:20 -0700

    use axios instead of request

--- controllers/twinword.js
+++ controllers/twinword.js
@@ -1,28 +1,35 @@
-const express = require('express');
-const db = require('../db/models')
-const isLoggedIn = require('../middleware/isLoggedIn');
+const express = require("express");
+const db = require("../db/models");
+const isLoggedIn = require("../middleware/isLoggedIn");
 const router = express.Router();
-const request = require('request');
+const axios = require("axios");
 
 router.use(isLoggedIn);
 
-router.get('/:word', (req, res) => {
-    let options = {
-        url: `https://wordsapiv1.p.rapidapi.com/words/${req.params.word}/definitions`,
+router.get("/:word", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://wordsapiv1.p.rapidapi.com/words/${req.params.word}/definitions`,
+      {
         headers: {
-            'X-RapidAPI-Key': process.env.X_RAPID_API_KEY
-        }
+          "X-RapidAPI-Key": process.env.X_RAPID_API_KEY,
+        },
+      },
+    );
+
+    if (response.status === 200) {
+      res.send(response.data);
+    } else if (response.status === 404) {
+      res.status(404).send("word not found");
     }
-    let callback = function(error, response, body) {
-        if (!error & response.statusCode === 200) {
-            res.send(body)
-        } else if (!error & response.statusCode === 404){
-            res.status(404).send('word not found')
-        } else {
-            res.status(500).send('An error occurred')
-        }
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      res.status(404).send("word not found");
+    } else {
+      res.status(500).send("An error occurred");
     }
-    request(options, callback)
-})
+  }
+});
 
 module.exports = router;
+
